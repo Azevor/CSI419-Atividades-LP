bccef6a5eeec0d63574fcc54243519febf8c8240
Azevor <gildotiago@gmail.com>
2020-09-10 22:34:21 -0300

    Inserção de proposições diversas

--- Atividades-Diversas/atividade1-prolog.pl
+++ Atividades-Diversas/atividade1-prolog.pl
@@ -1,18 +1,47 @@
-% Concatenar elementos da lista
+% Concatenar duas listas
+/* ENTRADA:
+* concat(Lista1, Lista2, Lista1_Lista2)
+*/
 concat([], L, L).
-concat([H|T], L, [H|D]) :- write(H), write(T), write(L), write(D), concat(T, L, D).
+concat([H|T1], L, [H|T2]) :-
+    concat(T1, L, T2).
 
-% Inverter lista
-flip([], []).
-flip([H|T], L) :- flip(T, _), concat(H, [H], L).
+% Inverter elementos da lista
+/* ENTRADA:
+* flipList(Lista_Normal, Lista_Invertida)
+*/
+flipList([], []).
+flipList([H|T], F) :-
+    flipList(T, L),
+    concat(L, [H], F).
 
-% Quantidade par na lista
+% Contar número de elementos da lista
+/* ENTRADA:
+* sizeList(Lista, Tamanho_Lista)
+*/
+sizeList([], 0).
+sizeList([_|T], S) :-
+    sizeList(T, C),
+    S is C+1.
+
+% Verificar se o número de elementos da lista é par
+/* ENTRADA:
+* countEven(Lista)
+*/
 countEven([]).
-countEven([a,a|T]) :- write(T), countEven(T).
+countEven([_, _|T]) :-
+    countEven(T).
 
-% Item pertence à lista
+% Verificar se um elemento pertence à lista
+/* ENTRADA:
+* isPart(Elemento, Lista)
+*/
 isPart(H, [H|_]).
-isPart(H, [_|T]) :- isPart(H, T).
+isPart(H, [_|T]) :-
+    isPart(H, T).
 
 % Inserir elemento na cabeça da lista
-insere(H, L, [H|L]).
+/* ENTRADA:
+* insertHead(Elemento, Lista, Lista_Com_Elemento)
+*/
+insertHead(X, L, [X|L]).
